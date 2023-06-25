bf48e2002a988b3f69e3a78e9698a8a91573f04f
GITHUB <muraliraj1994@gmail.com>
2023-06-25 13:46:43 +0530

    completed

---
+++ src/org/emp/EmployeeOne.java
@@ -0,0 +1,16 @@
+package org.emp;
+
+public class EmployeeOne {
+	
+	private void add() {
+		System.out.println("add");
+
+	}
+	
+	
+	public static void main(String[] args) {
+		 EmployeeOne E = new EmployeeOne();
+		 E.add();
+	}
+
+}
